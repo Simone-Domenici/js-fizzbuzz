4fe8be619f9f0c86ff59ac18a95e05dd3dbd4a53
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-15 14:18:37 +0200

    ADD multiples of 3 printing Fizz

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 // Stampare numeri da 1 a 100
 for ( let i = 0; i < 100; i++ ) {
     let num = i + 1
-    console.log(num)
-}
+    // console.log(num)
+    // per i multipli di 3 stampare "Fizz"
+    let rest3 = num % 3
+    let numThreeMultiples = rest3 === 0
+    // console.log(rest3,numThreeMultiples)
+    if (numThreeMultiples) {
+        console.log('Fizz')
+    } else 
+        console.log('-----')
+}   
