f26c47d5ac80df1abc6285dd0c205d0e16f74cda
Simone-Domenici <domenici.simone00@gmail.com>
2024-10-15 14:29:03 +0200

    ADD merging codes toghether

--- js/app.js
+++ js/app.js
@@ -5,16 +5,14 @@ for ( let i = 0; i < 100; i++ ) {
     // per i multipli di 3 stampare "Fizz"
     let rest3 = num % 3
     let numThreeMultiples = rest3 === 0
-    // console.log(rest3,numThreeMultiples)
-    if (numThreeMultiples) {
-        console.log('Fizz')
-    } else 
-        console.log('-----')
-    // per i multipli di 5 stampare "Buzz"
     let rest5 = num % 5
     let numFiveMultiples = rest5 === 0
-    if (numFiveMultiples) {
-        console.log('Buzz')
-    } else 
-        console.log('-----')
+    // console.log(rest3,numThreeMultiples)
+    if (numThreeMultiples && numFiveMultiples) {
+        console.log(num,'FizzBuzz')
+    } else if (numThreeMultiples) {
+        console.log(num,'Fizz')
+    } else if (numFiveMultiples) {
+        console.log(num,'Buzz')
+    }   
 }   
